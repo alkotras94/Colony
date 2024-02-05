286933ff0c359749dfd6f590e8b1eb6e5f02c61e
alkotras94 <104847958+alkotras94@users.noreply.github.com>
2024-02-05 21:31:05 +0500

    init comit project

---
+++ Learn/Assets/Scripts/GameManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("Гейм менеджер")]
+    [Tooltip("Ресурсы ягод")] [SerializeField] private int _berries;
+
+    [Tooltip("Лист с крестьянами без работы")] [SerializeField] private List<MovingII> _peasant;
+    [Tooltip("Лист с крестьянами, с путем к камню")] [SerializeField] private List<MovingII> _peasantStone = new List<MovingII>();
+    [Tooltip("Лист с крестьянами, с путем к дереву")] [SerializeField] private List<MovingII> _peasantWood = new List<MovingII>();
+    [Tooltip("Лист с крестьянами, с путем к еде")] [SerializeField] private List<MovingII> _peasantFood = new List<MovingII>();
+    [Tooltip("Шаблон с крестьянами")] [SerializeField] private GameObject _templateAnt; 
+    [Tooltip("Место создания крестьян")] [SerializeField] private Transform _targetAnt;
+
+    [Tooltip("Координаты склада")] public Transform Home; // Склад, дом
+    [Tooltip("Место сбора после создания")] public Transform ColectingPeasant; // Место сбора после создания крестьян и не работающих крестьян
+    [Tooltip("Источник еды")] public Transform FoodSource; // Источник еды
+    [Tooltip("Источник камня")] public Transform SheetSource; // Источник камня
+    [Tooltip("Источник дерева")] public Transform WoodSource; // Источник дерева
+
+    //Счетчики для ресурсов
+    public int ForFood; //На еду
+    public int OnTheWood; //Кто пойдет на дерево
+    public int OnStones; //Кто пойдет на камень
+    float x = 0.2f;
+
+    public static GameManager Instance { get; set; }
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if(Instance == this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void AddAnt() // добавление новых крестьян
+    {
+        GameObject templateAnt = Instantiate(_templateAnt, _targetAnt.position, Quaternion.identity);
+        _peasant.Add(templateAnt.GetComponent<MovingII>()); ;
+    }
+
+    public void ChangeTheSourceToStone() //Поменять источник на камень
+    {
+        for (int i = 0; i < OnStones; i++)
+        {
+            _peasant[i].Speed = 0.2f;
+            _peasant[i].TargetIstochnic = SheetSource;
+            _peasantStone.Add(_peasant[i]);
+        }
+        _peasant.RemoveRange(0, OnStones);
+    }
+
+    public void ChangeTheSourceToWood() //Поменять источник на дерево
+    {
+        for (int i = 0; i < OnTheWood; i++)
+        {
+            _peasant[i].Speed = 0.2f;
+            _peasant[i].TargetIstochnic = WoodSource;
+            _peasantWood.Add(_peasant[i]);
+        }
+        _peasant.RemoveRange(0, OnTheWood);
+    }
+
+}
---
+++ Learn/Assets/Scripts/MovingII.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingII : MonoBehaviour
+{   //Скрипт передвижения персонажей и хранения ресурсов у крестьян
+    public float Speed; // Скорость юнита
+    [SerializeField] private int _resours; // временная ячейка для хранения переноски листа 
+    public Transform TargetSclad; //Точка склада
+    public Transform TargetIstochnic = null; //Точка источника 
+    private Animator _animator;
+    bool _isMau = true;
+    public Transform Collecting; // Место сбора
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+        TargetSclad = GameManager.Instance.Home;
+        Collecting = GameManager.Instance.ColectingPeasant;
+    }
+    private void Update()
+    {
+
+        if (TargetIstochnic == null)
+        {
+            MoveTransform(Collecting);
+            //Debug.Log("Источника нет");
+        }
+        else
+        {
+            Moving();
+            //Debug.Log("Источник есть");
+        }
+            
+    }
+
+    //При входе в тригер персонаж останавливается, воспроизводит анимацию забирает ресурс и уходи на склад
+    //У обьекта должен быть коллайдер с галочуой Тригер
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Collecting")) 
+        {
+            Speed = 0;
+        }
+
+        if (collision.CompareTag("Home")) 
+        {
+            _resours = 0;
+        }
+
+        if (collision.CompareTag("ResoursSheet")) //Если попал в тригер камня
+        {
+            CollectingResourse();
+        }
+
+        if (collision.CompareTag("ResoursWood")) //Если попал в тригер дерева
+        {
+            CollectingResourse();
+        }
+
+
+    }
+
+    private void Moving()
+    {
+        if (_resours == 0)
+        {
+            TurnWithoutResources(TargetIstochnic); //поворот
+            if (_isMau)
+            {
+                MoveTransform(TargetIstochnic);
+            }
+        }
+        if (_resours == 1)
+        {
+            _animator.SetBool("AxeBool", false);
+            _isMau = true;
+            TurningWithResources(TargetIstochnic); //поворот
+            MoveTransform(TargetSclad);
+
+            /*if (transform.position == TargetSclad.position)
+            {
+                //_resoursList.AddList(_resours);
+                _resours = 0;
+                Debug.Log("Принес лист");
+            }*/
+        }
+    }
+
+    private void MoveTransform(Transform target) //Передвижение персонажа
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target.position, Speed * Time.deltaTime);
+    }
+    
+    public void TurnWithoutResources(Transform source) //Поворот обекта без ресурсов
+    {
+            if (transform.position.x > source.position.x)
+             {
+                transform.rotation = Quaternion.Euler(0,180,0); //поворот персонажа
+
+             }
+             else
+             {
+                 transform.rotation = Quaternion.Euler(0, 0, 0);
+             }
+    }
+
+    public void TurningWithResources(Transform source) //Поворот обекта с ресурсами
+    {
+            if (transform.position.x < source.position.x)
+            {
+                transform.rotation = Quaternion.Euler(0, 180, 0);
+            }
+            else
+            {
+                transform.rotation = Quaternion.Euler(0, 0, 0);
+            }
+    }
+
+    public void AddSource()
+    {
+        _resours = 1;
+    }
+
+    public void CollectingResourse() //Метод анимации, движения и поворота персонажа при сборе ресурсов
+    {
+        _isMau = false;
+        if (transform.position.x < GameManager.Instance.Home.position.x)
+        {
+            _animator.SetBool("AxeBool", true);
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+            Debug.Log("Забрал лист");
+        }
+        else
+        {
+            _animator.SetBool("AxeBool", true);
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+            Debug.Log("Забрал лист");
+        }
+    }
+}
